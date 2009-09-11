a42d4bc125a81b578f70cc7ab736375492bad309
sio4 <sio4@d767522a-b333-11de-a210-8fd6ca7157a7>
2009-09-11 03:54:55 +0000

    implemented basic structure and authentication (closes r67, r35)

--- trunk/ongsung/wall/views.py
+++ trunk/ongsung/wall/views.py
@@ -1,18 +1,72 @@
 # Create your views here.
 from wall.models import *
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 
 import os
+import datetime
 
+@login_required
 def index(request, template_name='wall/index.html'):
-	recent = Bookmark.objects.all().order_by('-last_date')[:10]
-	stared = Bookmark.objects.filter(stared=True).order_by('-count')[:10]
+	recent = Bookmark.objects.all().order_by('-last_date')[:5]
+	top = Bookmark.objects.all().order_by('-count')[:5]
+	stared = Bookmark.objects.filter(stared=True).order_by('-count')[:5]
+
+	try:
+		message = request.session['message']
+		request.session['message'] = ''
+	except:
+		message = ''
+
+	try:
+		error = request.session['error']
+		request.session['error'] = ''
+	except:
+		error = ''
+
 	return render_to_response(template_name,
-			{'recent':recent, 'stared':stared})
+			{'user':request.user, 'message':message, 'error':error,
+				'recent':recent, 'stared':stared, 'top':top})
 
 def connect(request):
-	os.system('ongsung-tunnel %s' % request.POST['addr'])
-	return HttpResponseRedirect('..')
+	user = request.user
+	requested_addr = request.POST['addr'].split()[0]
+	current_datetime = datetime.datetime.now()
+
+	try:
+		d = Device.objects.filter(addr=requested_addr)[0]
+		addr = d.addr
+		port = d.port
+		message = "device(%s) selected." % d.name
+	except:
+		error = "unknown device or invalid."
+		error += " contact to system administrator."
+
+		request.session['error'] = error
+		return HttpResponseRedirect(reverse('wall.views.index'))
+
+	try:
+		b = Bookmark.objects.filter(user=user,device=d)[0]
+		b.last_date = current_datetime
+		b.count += 1
+		message += ' visit again!'
+	except:
+		b = Bookmark(user=user,device=d,last_date=current_datetime)
+		message += ' grad to see you!'
+	b.save()
+
+	# templorary
+	port = 8000
+	os.system('ongsung-tunnel %s %s &' % (addr, port))
+
+	request.session['message'] = message
+	return HttpResponseRedirect(reverse('wall.views.index'))
+
+def stared(request,bookmark_id):
+	return HttpResponseRedirect(reverse('wall.views.index'))
+
