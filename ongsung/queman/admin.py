b60c5f7bf1ddf6c0873ee418cd7a756b44dad6ff
sio4 <sio4@d767522a-b333-11de-a210-8fd6ca7157a7>
2009-09-13 08:03:47 +0000

    fixes #76, base of queman, models and admin. (takes 2.5hours+)

---
+++ ongsung/queman/admin.py
@@ -0,0 +1,22 @@
+from queman.models import *
+from django.contrib import admin
+
+class FeatureAdmin(admin.ModelAdmin):
+	list_display = ('name','description')
+
+class WorkerAdmin(admin.ModelAdmin):
+	fields = ['name','addr','features']
+	list_display = ('name','addr')
+	list_filter = ['features']
+
+class QueueAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Basic' ,{'fields':['owner','command','feature','status']}),
+	]
+	list_display = ('command','owner','feature','status','worker','info',
+			'created_at','updated_at','started_at','finished_at')
+	list_filter = ['command','owner','feature','status','worker']
+
+admin.site.register(Feature,FeatureAdmin)
+admin.site.register(Worker,WorkerAdmin)
+admin.site.register(Queue,QueueAdmin)
