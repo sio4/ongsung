574c8590bbc72105b88e623e665aa1c5bc36fd2c
sio4 <sio4@d767522a-b333-11de-a210-8fd6ca7157a7>
2009-09-23 14:47:20 +0000

    add basic structure of 'logger' module. closes #40, refs #39.

---
+++ ongsung/logger/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+from django.dispatch import dispatcher
+from django.db.models import signals
+
+import uuid
+
+# Create your models here.
+
+class Host(models.Model):
+	name = models.CharField(max_length=80)
+	addr = models.IPAddressField(max_length=80)
+
+	def __unicode__(self):
+		return self.name
+
+
+
+class Session(models.Model):
+	time = models.DateTimeField(auto_now=True)
+	uuid = models.CharField(max_length=36)
+	host = models.ForeignKey(Host)
+	context = models.CharField(max_length=80)
+
+	def __unicode__(self):
+		return str(self.uuid)
+
+	def _set_uuid(self):
+		print "_set_uuid called"
+		self.uuid = str(uuid.uuid4())
+
+	def link_to(self, host_addr):
+		self._set_uuid()
+		try:
+			self.host = Host.objects.filter(addr=host_addr)[0]
+		except:
+			self.host = Host(name=host_addr,addr=host_addr)
+			self.host.save()
+		print "HOST: %s" % self.host
+
+
+
+PRIORITY_CHOICES = (
+	('C', 'Critical'),
+	('E', 'Error'),
+	('W', 'Warning'),
+	('N', 'Notice'),
+	('L', 'Log'),
+)
+
+class Log(models.Model):
+	"""
+	session:	reference to session of connect
+	statement:	log statement
+	serial:		serial number. can be same if continued statement
+	"""
+	session = models.ForeignKey(Session, null=True)
+	statement = models.CharField(max_length=256)
+	serial = models.IntegerField(default=1)
+	priority = models.CharField(max_length=1,
+			choices=PRIORITY_CHOICES, default='L')
+	time = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.time.strftime("%Y-%m-%d %H:%M:%S ") + self.priority + " " + self.statement
+
+
+# strange
+#def session_post_init(sender, instance, **kwargs):
+#	instance._set_uuid()
+#
+#signals.post_init.connect(session_post_init, sender=Session)
---
+++ ongsung/logger/views.py
@@ -0,0 +1,40 @@
+# Create your views here.
+
+from logger.models import *
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+
+
+def index(request, template='logger.html'):
+	logs = Log.objects.all()
+	return render_to_response(template, {'logs':logs})
+
+
+
+def insert(request):
+	st = request.POST.get('statement', '')
+	pr = request.POST.get('priority', 'L')
+	se = request.POST.get('serial', 0)
+	uu = request.POST.get('session', 'new')
+
+	content = "%s,%s,%s,%s" % (st,pr,se,uu)
+	print content
+
+	if (uu == 'new' or uu == ''):
+		ho = request.META.get('REMOTE_ADDR', '0.0.0.0')
+		co = request.POST.get('context', '')
+		session = Session(context=co)
+		session.link_to(ho)
+		session.save()
+	else:
+		session = Session.objects.filter(uuid=uu)[0]
+
+	log = Log(session=session, statement=st, serial=se, priority=pr)
+	log.save()
+
+	content = "%s %s xxx%s" % (str(session), se, content)
+	return HttpResponse(content=content, status=202)
+
+
