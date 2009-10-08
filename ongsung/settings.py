4fbe0b1f12d87c26b35b94c426cfdb0d5236faaa
sio4 <sio4@d767522a-b333-11de-a210-8fd6ca7157a7>
2009-10-08 14:34:01 +0000

    move some templates, impl. user management feature including C/R/D/.

--- ongsung/settings.py
+++ ongsung/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'ongsung.main',
     'ongsung.wall',
     'ongsung.queman',
     'ongsung.logger',
