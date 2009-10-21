7ca554422a20201f690f8d423fe507422be08e9b
sio4 <sio4@d767522a-b333-11de-a210-8fd6ca7157a7>
2009-10-21 07:18:46 +0000

    impl. user information editor for admin. refs #112

--- ongsung/main/urls.py
+++ ongsung/main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
-from django.views.generic.create_update import delete_object
+from django.views.generic.create_update import update_object,delete_object
 from django.contrib.auth.models import User
 
 from django.views.generic.list_detail import object_list
@@ -19,10 +19,12 @@ urlpatterns = patterns('',
 	#(r'^log/', object_list, dict(page, queryset=Log.objects.all())),
 	(r'^session/(?P<session_id>\w+)/$', 'logger.views.session_detail'),
 	#(r'^session/([^/]+)/$', 'logger.views.session_detail'),
+
 	(r'^user/$', 'main.views.user_index'),
 	(r'^user/create/$', 'main.views.user_create'),
+	(r'^user/(?P<user_id>\w+)/update/(.*)$', 'main.views.user_update'),
 	(r'^user/(?P<object_id>\w+)/delete/$', delete_object,
-		{'model':User,'post_delete_redirect':'/admin/user/'}),
+		{'model':User,'post_delete_redirect':'/admin/user/','login_required':True}),
 	(r'^user/(?P<user_id>\w+)/(.*)$', 'main.views.user_detail'),
 )
 
