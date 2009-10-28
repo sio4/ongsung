576c6a420ee00174b37baee7c9f41943b2cea5e1
sio4 <sio4@d767522a-b333-11de-a210-8fd6ca7157a7>
2009-10-28 04:17:38 +0000

    more messages from site-build.sh.

--- trunk/site-build.sh
+++ trunk/site-build.sh
@@ -6,13 +6,19 @@ django_dir="/var/lib/python-support/python$python_version/django"
 site_root=/home/moyang
 
 
-echo "install binaries..."
+echo -n "install binaries... "
+echo -n "mkdir, "
 mkdir -p $site_root/bin
+echo -n "build, "
 make tunneld > make.log 2>&1
+echo -n "tunneld, "
 install -m755 -s tunneld $site_root/bin/
+echo -n "ongsung-tunnel, "
 install -m755 ongsung/ongsung-tunnel $site_root/bin/
+echo -n "parse, "
 sed -i "s,@TUNNEL_EXEC_PATH@,$site_root/bin,;s,@TUNNEL_EXEC_NAME@,tunneld," \
 	$site_root/bin/ongsung-tunnel
+echo "done."
 
 echo "sync media for admin from dist..."
 rsync -az $django_dir/contrib/admin/media/ $site_root/media/
